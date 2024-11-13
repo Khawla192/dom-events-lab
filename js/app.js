49bc39fb9e87df4ee3b2942987f0de4ed6ae21b5
Khawla192 <202005171@stu.uob.edu.bh>
2024-11-13 20:32:01 +0300

    Solution

--- js/app.js
+++ js/app.js
@@ -1,9 +1,78 @@
 /*-------------------------------- Constants --------------------------------*/
 
+
+
 /*-------------------------------- Variables --------------------------------*/
+
+let firstNumber = ''
+let secondNumber = ''
+let mathOperator = null
+let result = 0
 
 /*------------------------ Cached Element References ------------------------*/
 
-/*----------------------------- Event Listeners -----------------------------*/
+const displayElement = document.querySelector('.display')
+const buttonsElement = document.querySelectorAll('.button')
+const equalButtonElement = document.getElementById('equal')
+const clear = document.getElementById("clear")
 
 /*-------------------------------- Functions --------------------------------*/
+
+const display = (event) => {
+    if (event.target.classList.contains('number'))  {
+        if (mathOperator === null){
+            firstNumber += event.target.innerText
+            displayElement.innerText = firstNumber
+        } else {
+            secondNumber += event.target.innerText
+            displayElement.innerText = firstNumber + mathOperator + secondNumber
+        }
+    } else if (event.target.classList.contains('operator'))  {
+        let selectedOperator = event.target.classList.contains('operator')
+        if (selectedOperator !== 'C') {
+            if (firstNumber !== '') {
+                mathOperator = event.target.innerText
+                displayElement.innerText = firstNumber + mathOperator
+                calculatorFunction()
+            }
+        } else {
+            displayElement.innerText = ''
+            firstNumber = ''
+            secondNumber = ''
+            mathOperator = null
+            result = 0
+        }
+    } else if (equalButtonElement)  {
+        calculatorFunction()
+    } 
+}
+
+const clearFunction = () => document.location.reload()
+
+const calculatorFunction = () => {
+    if (firstNumber !== '' && secondNumber !== '' && mathOperator) {
+        let num1 = parseFloat(firstNumber)
+        let num2 = parseFloat(secondNumber)
+
+        if (mathOperator === '+') {
+            result = num1 + num2
+            displayElement.innerText = mathOperator
+        } else if (mathOperator === '-') {
+            result = num1 - num2
+        } else if (mathOperator === '*') {
+            result = num1 * num2
+        } else if (mathOperator === '/') {
+            result = num1 / num2
+        } 
+
+        displayElement.innerText = result
+    }
+  }
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+buttonsElement.forEach((button) => {
+    button.addEventListener('click', display)
+})
+
+clear.addEventListener("click", clearFunction)
